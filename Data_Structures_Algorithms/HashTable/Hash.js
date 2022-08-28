8209e6a256ab37a0c07ee9e47dbe0db71bd798d3
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-08-28 18:02:46 +0530

    updating..

--- Data_Structures_Algorithms/HashTable/Hash.js
+++ Data_Structures_Algorithms/HashTable/Hash.js
@@ -2,7 +2,7 @@ class HashTable {
     constructor(size){
         this.data = new Array(size);
     }
-
+    //O(1)
     _hash(key){
         let hash = 0;
         for(let i=0;i<key.length;i++){
@@ -11,26 +11,44 @@ class HashTable {
         }
         return hash;
     }
+    //O(1)
+    //set method which receives a key and value
     set(key, value){
+        //createa variable we wanna store this.data at this.hash(key)
         let address = this._hash(key);
+        //if address doesnt exist(if nothing is there) 
         if (!this.data[address]) {
+            //simply add the data
             this.data[address] = [];
+            //push command
             this.data[address].push([key.value])
             console.log(this.data)
         } 
+        //returning always adding key and value
+        //else 
         this.data[address].push([key, value])
     }
+    //O(1)
+    //receive a key
     get(key){
+        //black box 
         let address = this._hash(key);
+        //pointing to the adress
         const currentBucket = this.data[address];
         console.log(currentBucket)
-        if(currentBucket){
+        //
+        if(currentBucket/*.length*/){
+            //loop over
+            //
             for(let i =0; i <currentBucket.length; i++){
+                //i is index zero equals key
                 if(currentBucket[i][0] === key){
-                    return currentBucket[i] [1];
+                    //return currentBucket 1
+                    return currentBucket[i][1];
                 }
             }
         }
+        //because there is nothing in the bucket
         return undefined;
     }
 }
---
+++ object_oriented_programming/class.js
@@ -0,0 +1,17 @@
+class Car {
+    constructor(name,year){
+        //initial properties
+
+        this.name=name;
+        this.year=year;
+    }
+    age(){
+        let date = new Date();
+        return date.getFullYear()- this.year;
+    }
+}
+let mycar1 = new Car("Ford",2000);
+let mycar2 = new Car("Audi",2019);
+console.log(mycar1);
+console.log(mycar2);
+console.log("My car is "+ mycar1.age()+"Years Old");
