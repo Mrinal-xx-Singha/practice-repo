d8de6f1335b86c5dd7107d128f81fd4c4002a36e
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-09-12 20:16:03 +0530

    string problem

---
+++ LeetcodeQuestions/ReverseAString.js
@@ -0,0 +1,12 @@
+var reverseString = function(s){
+
+
+    //made a variable and stored the reverse string 
+    var revArray = s.reverse();
+    //console logging to check the answer
+    console.log(revArray);
+
+    // returning the revarray answer
+    return revArray;
+}
+reverseString(['M','R','I','N','A','L',]);
