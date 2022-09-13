1ea77125ff354e955aaa3a9462771f6d704b387f
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-09-13 20:08:30 +0530

    updating the solution

--- LeetcodeQuestions/kidWithTheGreatestNumberOfCandy.js
+++ LeetcodeQuestions/kidWithTheGreatestNumberOfCandy.js
@@ -13,3 +13,35 @@ var kidsWithCandies = function(candies, extraCandies) {
        }
        return res;
    };
+   
+// Optimal Solution
+
+var kidsWithCandies = function(candies, extraCandies) {
+    // made an empty array
+    const res = [];
+    // found out the maxim array
+   const max = Math.max(...candies);
+//    looped for the length of candies
+   for(let i=0; i< candies.length; i++){
+    // made a variable named csum = candies[i] + extraCandies ;
+    // added the extraCandies to the array
+       const csum = candies[i] + extraCandies;
+    //    if the csum is greater than or equal to the max number
+       if(csum >= max){
+        // push true 
+           res[i]= true;
+       } else{
+        // else push false 
+           res[i]= false;
+       }
+   }
+//    return the result
+   return res;
+};
+
+
+
+
+
+
+
