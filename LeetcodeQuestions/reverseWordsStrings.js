524d3aa4be89c7c97546eb8a950b5fbc02748944
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-09-16 23:32:35 +0530

    reversing a word in a  string

---
+++ LeetcodeQuestions/reverseWordsStrings.js
@@ -0,0 +1,14 @@
+//revserse word in array
+
+
+var reverseWords = function(s) {
+    // made a new array
+    const resArr = []
+    // split s
+    const splitS = s.split(' ')
+    for (let i = 0; i < splitS.length; i++){
+        const word = splitS[i]
+        resArr.push(word.split('').reverse().join(''))
+    }
+    return resArr.join(' ')
+}
