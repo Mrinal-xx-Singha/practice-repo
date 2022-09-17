f1f075a545763c41d7eafdede410ac692f20540a
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-09-17 18:02:16 +0530

    firstRecurringCharacter

---
+++ LeetcodeQuestions/frc.js
@@ -0,0 +1,27 @@
+// first recurring charaacter
+// O(n^2)
+// function firstRcharacter(input){
+//     for(let i=0;i< input.length;i++){
+//         for(let j=i+1;i< input.length;j++){
+//         if(input[i] === input[j]) {
+//             return input[i];
+//             }
+//         }
+//     }
+//     return undefined;
+// }
+// O(n)
+function firstRcharacter2(input){
+    let map ={};
+    for(let i=0;i<input.length;i++){
+        if(map[input[i]] !==undefined){
+            return input[i]
+        } else {
+            map[input[i]] = i;
+        }
+        console.log(map)
+    }
+    return undefined;
+}
+
+console.log(firstRcharacter2([2,5,1,2,3,5,1,2,4]));
