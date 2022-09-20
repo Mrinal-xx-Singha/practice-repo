d81c57888f7b64e92e5de4837bde5cc0d80f8725
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-09-20 20:23:00 +0530

    reverse a ll

---
+++ LeetcodeQuestions/ReverseALinkedList.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var reverseList = function(head) {
+    // current pointer pointing to head
+    let curr = head;
+    // previous to null
+    let prev = null;
+    // next  to the next pointer 
+    let next;
+    //   while the current meaning the head is not null
+    // keep looping over  
+    while(curr !==null ){
+        // make the next variable the head 
+        next=curr.next;
+        // make the head null
+        curr.next=prev;
+        // make the null the previous 
+        prev=curr;
+        // make current the previous
+        curr = next;
+        
+    }
+    return prev;
+};
