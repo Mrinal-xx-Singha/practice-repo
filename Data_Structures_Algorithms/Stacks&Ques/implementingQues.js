7514fd8e7372844c2480272cc880bddb9330bf26
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-09-24 17:39:18 +0530

    increasingOrderSearchTree

---
+++ Data_Structures_Algorithms/Stacks&Ques/implementingQues.js
@@ -0,0 +1,36 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Ques {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.length =0;
+    }
+    peek(){
+        return this.first;
+    }
+    enque(value){
+        const newNode = new Node(value)
+        if(this.length===0){
+            this.first = newNode;
+            this.last = newNode;    
+        }
+        else{
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.length++;
+        return this.this;
+    }
+    
+}
+const myQueue = new Ques();
+myQueue.enque("joy");
+myQueue.enque("Sameer");
+myQueue.enque("Mrinal")
+console.log(myQueue);
