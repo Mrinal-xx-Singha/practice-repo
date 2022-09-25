fed05e45ea16f5f627ee1a999772995ccc1e5470
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-09-25 21:15:40 +0530

    maximum two sum of linked list

---
+++ LeetcodeQuestions/MaximumTwinSumOfLinkedList.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+ var pairSum = function(head) {
+    let ans = [];
+    while(head !=null){
+        ans.push(head.val);
+        head = head.next;
+    }
+    let max =0;
+    for(let i=0;i<ans.length;i++){
+        max = Math.max(max, (ans[i] +ans[ans.length-1-i]));
+    }
+    return max;
+};
