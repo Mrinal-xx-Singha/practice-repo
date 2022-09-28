424ccf668ca041721f6abc9f1798eb94b689e7a7
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-09-28 09:23:26 +0530

    updating the insertion sort

--- Data_Structures_Algorithms/Sorting/InsertionSort.js
+++ Data_Structures_Algorithms/Sorting/InsertionSort.js
@@ -3,18 +3,24 @@
 const number=[99,44,6,2,1,5,63,87,283,4,0];
 
 function insertionSort(array){
-    const length = array.length;
-        for(let i=0;i<length;i++){
-        if(array[i] < array[0]){
-        array.unshift(array.splice(i,1)[0]);
-    }else{
-        for(let j=1;j<i;j++){
-            if(array[i] > array[j-1] && array[i] <array[j]){
-                array.splice(j,0,array.splice(i,1)[0]);
+    const length =array.length;
+    for(let i=0;i<length;i++){
+        console.log(i,"i");
+        if(array[i]<array[0]){
+            console.log(array.unshift(array.splice(i,1)[0]));
+        } else {
+            for(let j=1;j<i ;j++){
+                console.log(j,"j");
+                if(array[i] > array[j-1] && array[i] < array[j]){
+                    console.log(array.splice(j,0,array.splice(i,1)[0]));
                 }
             }
         }
     }
 }
+insertionSort(number);
+console.log(number);
 
-console.log(insertionSort(number));
+
+// Time Complexity - o(n)
+// space complexity -o(1)
