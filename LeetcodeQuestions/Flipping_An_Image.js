d99bff465ae0ac974ea191bc96879ecfce60eedb
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-09-30 13:31:32 +0530

    leetcode #832

--- LeetcodeQuestions/Flipping_An_Image.js
+++ LeetcodeQuestions/Flipping_An_Image.js
@@ -1,33 +1,35 @@
-/**
- * @param {number[][]} image
- * @return {number[][]}
- */
- var flipAndInvertImage = function(image) {
-    let flipped = flip(image);
-    return invert(flipped)
-};
-// made a flip method
-flip =(matrix) => {
-    for(let i=0;i<matrix.length;i++){
-        let flipped =[];
-        for(let j=matrix[i].length-1;j >= 0;j--){
-            flipped.push(matrix[i][j])
-        }
-        matrix[i] = flipped;
-    }
-    return matrix;
-}
-// Made an invert method
-invert = (matrix) => {
-    for (let i=0;i<matrix.length;i++){
-        for(let j=0;j<matrix[i].length;j++){
-            if(matrix[i][j] === 0){
-                matrix[i].splice(j,1,1)
-            }
-            else if(matrix[i][j] === 1){
-                matrix[i].splice(j,1,0)
-            }
-        }
-    }
-    return matrix;
-}
+// ---------------------------------------OPTIMAL------------------------------------
+// /**
+//  * @param {number[][]} image
+//  * @return {number[][]}
+//  */
+//  var flipAndInvertImage = function(image) {
+//     let flipped = flip(image);
+//     return invert(flipped)
+// };
+// // made a flip method
+// flip =(matrix) => {
+//     for(let i=0;i<matrix.length;i++){
+//         let flipped =[];
+//         for(let j=matrix[i].length-1;j >= 0;j--){
+//             flipped.push(matrix[i][j])
+//         }
+//         matrix[i] = flipped;
+//     }
+//     return matrix;
+// }
+// // Made an invert method
+// invert = (matrix) => {
+//     for (let i=0;i<matrix.length;i++){
+//         for(let j=0;j<matrix[i].length;j++){
+//             if(matrix[i][j] === 0){
+//                 matrix[i].splice(j,1,1)
+//             }
+//             else if(matrix[i][j] === 1){
+//                 matrix[i].splice(j,1,0)
+//             }
+//         }
+//     }
+//     return matrix;
+// }
+// ----------------------MY SOLUTION--------------------
