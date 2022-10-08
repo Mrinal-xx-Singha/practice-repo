ad9d0ce9d8d4185ba634cc9fc865449d2e6ff911
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-10-08 15:49:28 +0530

    leetcode #338

---
+++ LeetcodeQuestions/CountingBits.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+ var countBits = function(n) {
+    let bits = [];
+    for(let i=0;i<=n;i++)
+        bits.push(Number(i).toString(2).replace(/0/g,'').length);
+        return bits
+};
