48f0ce07f6676e8281c1edc086c135aa242edf27
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-10-12 09:53:22 +0530

    leetcode #287

---
+++ LeetcodeQuestions/FindDuplicateNumber.js
@@ -0,0 +1,26 @@
+// Hint:
+
+// #1.
+// Think of model of linked list, and algorithm of cycle detection.
+// A common and practical one is Tortoise and Hare algorithm, also known as Floyd's algorithm.
+
+// #2.
+// Convert this challenge into cycle detection in linked list, and locating of start node of cycle.
+
+// Each array cell denotes a node in linked list.
+// And the value of each array cell points to the next node in linked list.
+
+// Finally, what we want to know is the duplicate number, which also is the start node of cycle, and vice versa.
+
+// We take the demo input as example, please refer to the diagram as following.
+
+// #3.
+
+// Remeber that description asks us not to modify the array.
+// (assume the array is read only).
+
+// Illustration:
+
+// Input:
+// nums = [1,3,4,2,2]
+// ------------------------------------------
