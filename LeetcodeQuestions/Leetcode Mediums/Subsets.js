d2d315352f8bf3a0a3f26ed4c4d55ac863b54126
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-10-18 11:42:43 +0530

    leetcode #78

--- LeetcodeQuestions/Leetcode Mediums/Subsets.js
+++ LeetcodeQuestions/Leetcode Mediums/Subsets.js
@@ -5,7 +5,7 @@
 // The solution set must not contain duplicate subsets. Return the solution in any order
 
 
-Example 1:
+// Example 1:
 
 Input: nums = [1,2,3]
 Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
