0e7577a28e9d5959f79a7ad666f505f77b927d3c
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-11-03 21:07:49 +0530

    Leetcode #clscls

--- LeetcodeQuestions/Leetcode Mediums/InterValListIntersection.js
+++ LeetcodeQuestions/Leetcode Mediums/InterValListIntersection.js
@@ -13,3 +13,24 @@
 
 // Input: firstList = [[0,2],[5,10],[13,23],[24,25]], secondList = [[1,5],[8,12],[15,24],[25,26]]
 // Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+
+
+/**
+ * @param {number[][]} firstList
+ * @param {number[][]} secondList
+ * @return {number[][]}
+ */
+ var intervalIntersection = function(firstList, secondList) {
+    let Ai = 0,Bi = 0;
+    let res = []
+while(Ai < firstList.length && Bi <secondList.length){
+    let maxStart = Math.max(firstList[Ai][0],secondList[Bi][0]);
+    let minEnd = Math.min(firstList[Ai][1],secondList[Bi][1]);
+    
+    if(maxStart <=minEnd) res.push([maxStart,minEnd])
+    
+    if(firstList[Ai][1] < secondList[Bi][1])Ai++;
+    else Bi++
+}
+    return res;
+};
