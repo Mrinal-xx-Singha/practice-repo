0c31fdc3077ec418d5a03c2769258793e8964f08
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-12-23 22:37:24 +0530

    Leetcode #2363

---
+++ LeetcodeQuestions/MergeSimilarItems.js
@@ -0,0 +1,39 @@
+// You are given two 2D integer arrays, items1 and items2, representing two sets of items. Each array items has the following properties:
+
+// items[i] = [valuei, weighti] where valuei represents the value and weighti represents the weight of the ith item.
+// The value of each item in items is unique.
+// Return a 2D integer array ret where ret[i] = [valuei, weighti], with weighti being the sum of weights of all items with value valuei.
+
+// Note: ret should be returned in ascending order by value.
+
+ 
+
+// Example 1:
+
+// Input: items1 = [[1,1],[4,5],[3,8]], items2 = [[3,1],[1,5]]
+// Output: [[1,6],[3,9],[4,5]]
+// Explanation: 
+// The item with value = 1 occurs in items1 with weight = 1 and in items2 with weight = 5, total weight = 1 + 5 = 6.
+// The item with value = 3 occurs in items1 with weight = 8 and in items2 with weight = 1, total weight = 8 + 1 = 9.
+// The item with value = 4 occurs in items1 with weight = 5, total weight = 5.  
+// Therefore, we return [[1,6],[3,9],[4,5]].
+
+
+/*
+ * @param {number[][]} items1
+ * @param {number[][]} items2
+ * @return {number[][]}
+ */
+var mergeSimilarItems = function(items1, items2) {
+    let vwMap = new Map() //value weight
+
+
+    for(let [value,weight] of items1.concat(items2)){
+        if(!vwMap.has(value)){
+            vwMap.set(value,weight)
+        }else {
+            vwMap.set(value, vwMap.get(value) + weight)
+        } 
+    } 
+    return Array.from(vwMap.entries()).sort((a,b) => a[0] - b[0])
+};
