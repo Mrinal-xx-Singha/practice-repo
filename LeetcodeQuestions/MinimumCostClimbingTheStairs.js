de038f99362cd00b0dc8cfbb5ec75718dc1e6b8b
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-12-25 22:07:58 +0530

    Leetcode #1475

--- LeetcodeQuestions/MinimumCostClimbingTheStairs.js
+++ LeetcodeQuestions/MinimumCostClimbingTheStairs.js
@@ -13,3 +13,16 @@
 // Explanation: You will start at index 1.
 // - Pay 15 and climb two steps to reach the top.
 // The total cost is 15.
+
+
+/*
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    let n = cost.length;
+    for(let i=n-3;~i;i--)
+        cost[i] +=Math.min(cost[i+1],cost[i+2])
+
+    return Math.min(cost[0],cost[1])
+};
